4a2c83aa260a1bc7c37cf9f63f1caae76f40f354
Carlos Mendes <carls.mendes@gmail.com>
2017-08-11 10:14:37 +0100

    image upload with attach

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   resources :products
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  mount Attachinary::Engine => "/attachinary"
+
 end
